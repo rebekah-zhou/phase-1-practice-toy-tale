d9587dfd01af5aee869f09501ad41972dd19e271
Rebekah Zhou <rebekahzhou@gmail.com>
2022-01-22 12:37:36 -0800

    create toy card

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
+
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
+
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
     addToy = !addToy;
@@ -12,4 +14,36 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  fetch('http://localhost:3000/toys')
+    .then(res => res.json())
+    .then(toys => {
+      const toyCollection = document.querySelector('#toy-collection')
+      toys.forEach(toy => {
+        toyCard = createToyCard(toy)
+        toyCollection.append(toyCard)
+      })
+       
+      function createToyCard(obj) {
+        const h2Name = document.createElement('h2')
+        const cardDiv = document.createElement('div')
+        const imgAvatar = document.createElement('img')
+        const pLikes = document.createElement('p')
+        const buttonId = document.createElement('button')
+
+        cardDiv.className = 'card'
+        imgAvatar.src = obj.image
+        imgAvatar.className = 'toy-avatar'
+        h2Name.textContent = obj.name
+        pLikes.textContent = `${obj.likes} likes`
+        buttonId.className = 'like-btn'
+        buttonId.id = obj.id
+        buttonId.textContent = 'Like ❤️'
+        cardDiv.append(h2Name, imgAvatar, pLikes, buttonId)
+        console.log(cardDiv)
+        return cardDiv
+      }
+    })
+    
+
 });
